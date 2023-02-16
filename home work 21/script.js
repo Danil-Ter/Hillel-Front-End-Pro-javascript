8e5c6d51b76b9236cbf1d7073c6ee36b37b9b253
Danya-molodoy <114100824+Danya-molodoy@users.noreply.github.com>
2023-02-16 19:10:53 +0300

    fixed hm-21 comments

--- home work 21/script.js
+++ home work 21/script.js
@@ -1,14 +1,20 @@
+const input = document.querySelector(".input");
+const addTopBtn = document.querySelector(".btn-add-beginning");
+const addBottomBtn = document.querySelector(".btn-add-end");
+const deleteBtn = document.querySelector(".btn-delete");
+const sortBtn = document.querySelector(".btn-sort");
+const ul = document.querySelector(".ul");
+let lastHighlighted = null;
+
 ul.addEventListener("click", (event) => {
   const target = event.target;
   if (!target.matches(".list_item")) return;
 
-  if (event.ctrlKey) {
+  if (event.ctrlKey || event.metaKey) {
     target.classList.toggle("highlighted");
     lastHighlighted = target;
   } else if (event.shiftKey) {
-    const start = Array.from(ul.children).indexOf(
-      lastHighlighted || ul.children[0]
-    );
+    const start = lastHighlighted !== null ? Array.from(ul.children).indexOf(lastHighlighted) : 0;
     const end = Array.from(ul.children).indexOf(target);
     Array.from(ul.children)
       .slice(Math.min(start, end), Math.max(start, end) + 1)
@@ -22,12 +28,6 @@ ul.addEventListener("click", (event) => {
     lastHighlighted = target;
   }
 });
-
-const addTopBtn = document.querySelector(".btn-add-beginning");
-const addBottomBtn = document.querySelector(".btn-add-end");
-const deleteBtn = document.querySelector(".btn-delete");
-const sortBtn = document.querySelector(".btn-sort");
-const input = document.querySelector(".input");
 
 addTopBtn.addEventListener("click", () => {
   const li = document.createElement("li");
@@ -53,13 +53,9 @@ deleteBtn.addEventListener("click", () => {
 
 sortBtn.addEventListener("click", () => {
   const list = Array.from(ul.children);
-  list.sort((a, b) => {
+  list.sort((a) => {
     if (a.classList.contains("highlighted")) {
       return -1;
-    } else if (b.classList.contains("highlighted")) {
-      return 1;
-    } else {
-      return a.textContent.localeCompare(b.textContent);
     }
   });
   ul.innerHTML = "";
