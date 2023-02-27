6170bf356ae889d99769c6cea7dce9f2488d9eb9
Danya-molodoy <114100824+Danya-molodoy@users.noreply.github.com>
2023-02-27 10:43:40 +0300

    fixed 24 homework comments

--- home work 24/script.js
+++ home work 24/script.js
@@ -1,4 +1,5 @@
-const productsApiUrl = "https://fakestoreapi.com/products";
+const productsApiUrl =
+  "https://api.escuelajs.co/api/v1/products/?offset=0&limit=10";
 const productsContainer = document.querySelector(".products-container");
 const productDetails = document.querySelector(".product-details");
 
@@ -6,9 +7,8 @@ function fetchProducts() {
   fetch(productsApiUrl)
     .then((response) => response.json())
     .then((data) => {
-      productsContainer.innerHTML = ""; 
-      const shuffledData = shuffle(data);
-      shuffledData.slice(0, 10).forEach((product) => {
+      productsContainer.innerHTML = "";
+      data.forEach((product) => {
         const productCard = createProductCard(product);
         productsContainer.appendChild(productCard);
       });
@@ -16,36 +16,32 @@ function fetchProducts() {
     .catch((error) => console.error(error));
 }
 
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
 function createProductCard(product) {
   const productCard = document.createElement("div");
   productCard.classList.add("product-card");
   productCard.innerHTML = `
-    <img class=".product-img" src="${product.image}" alt="${product.title}">
+    <img class=".product-img" src="${product.images[0]}" alt="${product.title}">
     <div class="product-name">${product.title}</div>
     <div class="product-price">$${product.price.toFixed(2)}</div>
     <button class="add-to-cart">Add to Cart</button>
   `;
-  productCard.addEventListener("click", () => {
-    renderProductDetails(product);
+
+  productsContainer.addEventListener("click", (event) => {
+    if (event.target.closest(".product-card") === productCard) {
+      renderProductDetails(product);
+    }
   });
+
   return productCard;
 }
 
 function renderProductDetails(product) {
   productDetails.innerHTML = `
     <h2>${product.title}</h2>
-    <img src="${product.image}" alt="${product.title}">
+    <img src="${product.images[0]}" alt="${product.title}">
     <div class="details-price">$${product.price.toFixed(2)}</div>
     <p>${product.description}</p>
-    <p>Category: ${product.category}</p>
+    <p>Category: ${product.category.name}</p>
   `;
 }
 
