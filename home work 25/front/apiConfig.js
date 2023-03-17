2379020a64132e9c41e4f0a10632806b2d39482b
Danil Ter <danilater0150@gmail.com>
2023-03-17 19:21:18 +0300

    hm/25 fixed comments

---
+++ home work 25/front/apiConfig.js
@@ -0,0 +1,3 @@
+export const SERVER = 'http://localhost:3000';
+export const GET_PRODUCTS = '/api/products';
+export const PRODUCT_ID = (id) => `/api/products/${id}`;
--- home work 25/front/main.js
+++ home work 25/front/main.js
@@ -1,28 +1,36 @@
+import { SERVER, GET_PRODUCTS, PRODUCT_ID } from './apiConfig.js';
+
 const productsContainer = document.querySelector(".products-container");
 const productDetailsContainer = document.querySelector(".product-details");
 
-async function displayProducts() {
-  const response = await fetch("http://localhost:3000/api/products");
-  const { data: products } = await response.json();
-  const firstTenProducts = products.slice(0, 10);
-  productsContainer.innerHTML = firstTenProducts
-    .map(
-      (product) => `
-    <div class="product-card" data-id="${product.id}">
-      <img class="product-image" src="${product.images}">
-      <div class="product-wrapper-name">
-        <p class="product-name">${product.title}</p>
-      </div>
-      <div class="product-wrapper-price">
-        <p class="product-price">$${product.price}</p>
-      </div>
-      <div class="product-wrapper-button">
-        <button class="product-button">Delete</button>
-      </div>
-    </div>
-  `
-    )
-    .join("");
+function displayProducts() {
+  fetch(SERVER + GET_PRODUCTS)
+    .then(response => response.json())
+    .then(({ data: products }) => {
+      const firstTenProducts = products.slice(0, 10);
+      productsContainer.innerHTML = firstTenProducts
+        .map(
+          (product) => `
+        <div class="product-card" data-id="${product.id}">
+          <img class="product-image" src="${product.images}">
+          <div class="product-wrapper-name">
+            <p class="product-name">${product.title}</p>
+          </div>
+          <div class="product-wrapper-price">
+            <p class="product-price">$${product.price}</p>
+          </div>
+          <div class="product-wrapper-button">
+            <button class="product-delete-button ">Delete</button>
+          </div>
+        </div>
+      `
+        )
+        .join("");
+    })
+    .catch(error => {
+      console.error(error);
+      productsContainer.innerHTML = "<p>An error occurred while fetching the products. Please try again later.</p>";
+    });
 }
 
 function displayProductInfo(product) {
@@ -38,34 +46,54 @@ function displayProductInfo(product) {
   productDetailsContainer.innerHTML = ProductInfo;
 }
 
+function handleDeleteProduct(productId) {
+  fetch(
+    `${SERVER}${PRODUCT_ID(productId)}`,
+    { method: "DELETE" }
+  )
+    .then(response => {
+      if (response.ok) {
+        const productCard = document.querySelector(`.product-card[data-id="${productId}"]`);
+        productCard.remove();
+        if (productDetailsContainer.dataset.productId === productId) {
+          productDetailsContainer.innerHTML = '';
+          productDetailsContainer.dataset.productId = '';
+        }
+      } else {
+        throw new Error("Failed to delete product");
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      alert("An error occurred while deleting the product. Please try again later.");
+    });
+}
+
 async function handleProductClick(event) {
   const productCard = event.target.closest(".product-card");
   if (!productCard) return;
-  if (event.target.classList.contains("product-button")) {
-    await fetch(
-      `http://localhost:3000/api/products/${productCard.dataset.id}`,
-      { method: "DELETE" }
-    );
-    productCard.remove();
+  const productId = productCard.dataset.id;
+  if (event.target.classList.contains("product-delete-button")) {
+    await handleDeleteProduct(productId);
   } else {
-    const response = await fetch(
-      `http://localhost:3000/api/products/${productCard.dataset.id}`
-    );
-    const { data: product } = await response.json();
-    displayProductInfo(product);
+    fetch(`${SERVER}${PRODUCT_ID(productId)}`)
+      .then(response => response.json())
+      .then(({ data: product }) => {
+        displayProductInfo(product);
+        productDetailsContainer.dataset.productId = productId;
+      })
+      .catch(error => {
+        console.error(error);
+        alert("An error occurred while fetching the product details. Please try again later.");
+      });
   }
 }
 
-async function init() {
-  try {
-    await displayProducts();
-    const container = document.querySelector(".container");
-    container.addEventListener("click", handleProductClick);
-  } catch (error) {
-    console.error(error);
-    productsContainer.innerHTML =
-      "<p>An error occurred while fetching the products. Please try again later.</p>";
-  }
+function init() {
+  displayProducts();
+  const container = document.querySelector(".container");
+  container.addEventListener("click", handleProductClick);
 }
 
 init();
+
